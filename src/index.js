bfad6fc724e189c10f95a58809d5451dbd45ad21
andrpoch <andrpoch25@gmail.com>
2022-04-14 14:57:30 +0300

    Add

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+import ReactDOM from 'react-dom';
+import  App  from 'components/App';
 import './index.css';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
